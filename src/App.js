df43fd49791d7a4fa7a9bd0e22446b01efb246cb
Jacob Lang <jacoblang72@comcast.net>
2023-08-03 12:53:34 -0400

    addedcomps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Reactstrap from 'reactstrap';
+import styled from 'styled-components';
+import { Keyframes } from "styled-components/dist/types";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Team Builder
     </div>
   );
 }
---
+++ src/TeamForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+import axios from "axios";
+import Reactstrap from 'reactstrap';
+import styled from 'styled-components';
+import { Keyframes } from "styled-components/dist/types";
